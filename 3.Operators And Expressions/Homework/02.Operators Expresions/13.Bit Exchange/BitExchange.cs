4bf77a5a71ada394999e6151335d46fa7772bfaf
Kostadin Dinkov <koni.dinkov@gmail.com>
2013-05-22 01:29:21 +0300

    p14

--- 3.Operators And Expressions/Homework/02.Operators Expresions/13.Bit Exchange/BitExchange.cs
+++ 3.Operators And Expressions/Homework/02.Operators Expresions/13.Bit Exchange/BitExchange.cs
@@ -14,19 +14,28 @@ class BitExchange
     static void Main()
     {
         
-        int inputInt = int.Parse(Console.ReadLine());
-        string inputStr = Convert.ToString(inputInt,2).PadLeft(32,'0');
-        int[] inputArray = inputStr.Select(c => int.Parse(c.ToString())).ToArray();
+        uint inputInt = uint.Parse(Console.ReadLine());                               //Read the input number
+        string inputStr = Convert.ToString(inputInt,2).PadLeft(32,'0');             //Convert Input to binary string representation with leading zeros 
+        int[] inputArray = inputStr.Select(c => int.Parse(c.ToString())).ToArray(); //Create an array of ints from the string; example: {0,0,0,1,0,...,1,0}
         
-        PrintValues(inputArray);
+        PrintValues(inputArray);                                                    //Display the values of the array                                                  
 
-        int[] positionsA = { 3, 4, 5 };
-        int[] positionsB = { 24, 25, 26 };
+        int[] positionsA = { 3, 4, 5 };                                             //Array for storing the first sequence of bit positions to be swaped.
+        int[] positionsB = { 24, 25, 26 };                                          //Array for storing the second sequence of bit positions to be swaped.
         
-        int[] resultArray = (int[])inputArray.Clone();
-        Array.Copy(inputArray, Math.Abs( positionsA[2]-31), resultArray, Math.Abs(positionsB[2]-31), 3);
-        Array.Copy(inputArray, Math.Abs(positionsB[2]-31), resultArray, Math.Abs(positionsA[2]-31), 3);
+        int[] resultArray = (int[])inputArray.Clone();                              //Creating a copy of the input number array
+
         
+        int arrayPosA = Math.Abs(positionsA[2] - 31);                    //Since the indexing of an Array starts from left to right and we count bits in numbers
+        int arrayPosB = Math.Abs(positionsB[2] - 31);                    //from right to left a simple adjusting is needed.Note also that we use only the last index in
+                                                                         //the positions arrays - 5 and 26.That is because the position sequences are successive.
+
+        //Using the Array.Copy method .It works like that : Copies a sequence of successive indexes from the source array and places them at a given position(index) in the target array.
+        //Format: Array.Copy(sourceArray,sourceIdex,targetArray,targetIndex,int) where int is the lenght of the sequence of indexes to be copied.
+        
+        Array.Copy(inputArray, arrayPosA, resultArray, arrayPosB, 3);  
+        Array.Copy(inputArray, arrayPosB, resultArray, arrayPosA, 3);     
+                                                                                                                
         PrintValues(resultArray);
         
        
---
+++ 3.Operators And Expressions/Homework/02.Operators Expresions/14.Custom Bit Exchnge/CustomBitExchange.cs
@@ -0,0 +1,49 @@
+﻿/*
+ * Lecture 3 : Operators and Expressions
+ * Problem 14: Write a program that exchanges bits {p, p+1, …, p+k-1) 
+ *             with bits {q, q+1, …, q+k-1} of given 32-bit unsigned integer.
+ */
+
+
+using System;
+using System.Linq;
+
+class BitExchange
+{
+    static void Main()
+    {
+        Console.Write("Input a number:");
+        uint inputInt = uint.Parse(Console.ReadLine());                               //Read the input number
+        Console.Write("Input the first bit position to be exchanged: ");
+        int positionP = int.Parse(Console.ReadLine());                                //Read the first bit sequence position  
+        Console.Write("Input the second bit position to be exchanged: ");
+        int positionQ = int.Parse(Console.ReadLine());                                //Read the second bit sequence position  
+        Console.Write(@"Enter the how many successive bit you want \n
+                        to exchange between positions:");
+        int seqLen = int.Parse(Console.ReadLine());                                   //Read the lenght of the bit position sequence  
+       
+        string inputStr = Convert.ToString(inputInt, 2).PadLeft(32, '0');           //Convert Input to binary string representation with leading zeros 
+        int[] inputArray = inputStr.Select(c => int.Parse(c.ToString())).ToArray(); //Create an array of ints from the string; example: {0,0,0,1,0,...,1,0}
+        PrintValues(inputArray);                                                    //Display the values of the array                                                  
+        int[] resultArray = (int[])inputArray.Clone();                              //Creating a copy of the input number array
+        
+        positionP = Math.Abs((positionP) - 31) - seqLen+1;                          //The positions adapted to corespond to the right indexes in the array
+        positionQ = Math.Abs((positionQ) - 31) - seqLen+1;                          
+        
+        Array.Copy(inputArray,positionQ, resultArray, positionP, seqLen);          //Using  the Array.Copy method as decribed in Problem 13.
+        Array.Copy(inputArray,positionP, resultArray, positionQ, seqLen);          //Note that exception will be thrown if the sum  of any of  the starting positions and the sequence lenght is   
+                                                                                   //greater than 32 wich is the array's  and int type's lenght.                                                            
+        PrintValues(resultArray);
+
+
+    }
+    //a helper function for displaying the values of an Array
+    public static void PrintValues(Array myArr)
+    {
+        foreach (int i in myArr)
+        {
+            Console.Write(i);
+        }
+        Console.WriteLine();
+    }
+}
