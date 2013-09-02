de0a0b7cf18340cd109b6caf69fd6bae115c46a9
Kostadin Dinkov <koni.dinkov@gmail.com>
2013-09-02 12:21:22 +0300

    update

---
+++ CSharpPart2/08.Strings and Text Processing/Problem12ParseURL/MainClass.cs
@@ -0,0 +1,29 @@
+﻿/*
+ * Telerik Academy  - Homework Solutions
+ * Course   : C# Part II
+ * Lecture 8: Strings and Text processing
+ * Problem 12:Write a program that parses an URL address given in the format:
+ *              [protocol]://[server]/[resource]
+ *              and extracts from it the [protocol], [server] and [resource] 
+ *              elements. For example from the URL http://www.devbg.org/forum/index.php 
+ *              the following information should be extracted:
+ *              [protocol] = "http"
+ *              [server] = "www.devbg.org"
+ *              [resource] = "/forum/index.php"
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem12ParseURL
+{
+    class MainClass
+    {
+        static void Main()
+        {
+        }
+    }
+}
