f263c7306888af67185f15d51200696ff055dcd0
Kostadin Dinkov <koni.dinkov@gmail.com>
2014-08-28 01:45:04 +0300

    Defining classes -  lecture homeworks

---
+++ OOP/Drfining Classes Part I/GSM/Display.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GSM
+{
+    public class Display
+    {
+        private float size;
+        private string numberOfColors = null;
+
+        public Display(int size)
+            : this(size, null)
+        {
+        }
+
+        public Display(float size, string numberOfColors)
+        {
+            this.size = size;
+            this.numberOfColors = numberOfColors;
+        }
+
+        
+
+        public float Size
+        {
+            get
+            {
+                return this.size;
+            }
+
+            set
+            {
+                if (value >= 0 || value == null)
+                {
+                    this.size = value;
+                }
+                else
+                {
+                    throw new ArgumentException();
+                }
+            }
+        }
+
+        public string NumberOfColors
+        {
+            get
+            {
+                return this.numberOfColors;
+            }
+
+            set
+            {
+                
+                this.numberOfColors = value;
+                
+                
+            }
+        }
+
+        
+        public override string ToString()
+        {
+            StringBuilder endText = new StringBuilder();
+            endText.AppendLine("Display:");
+            endText.AppendLine(string.Format("Size: {0}",size.ToString()));
+            endText.AppendLine(string.Format("Number of Colors: {0}",this.numberOfColors));
+            return endText.ToString();
+        }
+    }
+}
